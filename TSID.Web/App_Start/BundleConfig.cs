881a908807214c0a059e2e51c9b8ec9b2c7430f5
threesisters <admin1@tsidweb.org>
2013-05-10 17:09:50 -0700

    Add shell TSIDWeb project

---
+++ TSID.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web.Optimization;
+
+namespace TSID.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();
+            AddDefaultIgnorePatterns(bundles.IgnoreList);
+
+            bundles.Add(
+                new ScriptBundle("~/scripts/modernizr")
+                .Include("~/scripts/modernizr-{version}.js"));
+
+            bundles.Add(
+                new ScriptBundle("~/scripts/vendor")
+                .Include("~/scripts/jquery-{version}.min.js")
+                .Include("~/scripts/bootstrap.min.js"));
+
+            bundles.Add(
+                new StyleBundle("~/Content/css")
+                .Include("~/Content/bootstrap.min.css")
+                .Include("~/Content/bootstrap-responsive.min.css")
+                .Include("~/Content/font-awesome.min.css")
+                .Include("~/Content/styles.css"));
+
+        }
+
+        private static void AddDefaultIgnorePatterns(IgnoreList ignoreList)
+        {
+            if (ignoreList == null)
+            {
+                throw new ArgumentNullException("ignoreList");
+            }
+
+            ignoreList.Ignore("*.intellisense.js");
+            ignoreList.Ignore("*-vsdoc.js");
+            //ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
+            //ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
+            //ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
+        }
+    }
+}
